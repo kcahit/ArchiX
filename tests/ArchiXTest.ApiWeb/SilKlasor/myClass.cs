7f9034432e38aeef75d179fa1b8727449e47118a
Cahit Kalkan <kcahit@gmail.com>
2025-09-12 13:10:10 +0300

    twst 01

--- tests/ArchiXTest.ApiWeb/SilKlasor/myClass.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ArchiXTest.ApiWeb._klasorSil
-{
-    public class myClass
-    {
-        // This is a test class for the _klasorSil namespace
-        // It currently has no properties or methods
-        // Future implementations can be added here
-    }
-}
