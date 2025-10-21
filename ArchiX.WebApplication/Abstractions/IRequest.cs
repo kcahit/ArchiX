7ae6ee2b6ec249eb84405bd519556a539e40a099
Cahit Kalkan <kcahit@gmail.com>
2025-10-21 13:23:05 +0300

    7,0100  ArchiX.WebApplication çalışmaları başladı

---
+++ ArchiX.WebApplication/Abstractions/IRequest.cs
@@ -0,0 +1,11 @@
+﻿// File: ArchiX.WebApplication/Abstractions/IRequest.cs
+namespace ArchiX.WebApplication.Abstractions
+{
+    /// <summary>
+    /// Represents a request that produces a response handled by a corresponding handler.
+    /// </summary>
+    /// <typeparam name="TResponse">The response type returned by the handler.</typeparam>
+    public interface IRequest<out TResponse>
+    {
+    }
+}
