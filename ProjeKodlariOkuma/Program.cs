c99247383a1f764a088d01f36f8a87c78e3615bd
Cahit Kalkan <kcahit@gmail.com>
2025-10-26 16:27:40 +0300

    peogram c s ok

--- ProjeKodlariOkuma/Program.cs
+++ ProjeKodlariOkuma/Program.cs
@@ -1,10 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-var kokDizin = @"C:\_git\ArchiX\Dev\ArchiX";
+var kokDizin = @"C:\_git\ArchiX\Dev\ArchiX"; 
 var uzantilarArr = new[] { ".cs", ".csproj" };
-var hedefDizin = @"C:\_git\ArchiX\notlarim";
-var format = "json";
+//var hedefDizin = @"C:\_git\ArchiX\notlarim";
+//var format = "json";
 
- ProjeKodlariOkuma.ProjeKodlariOkuma.KodlariOLustur(kokDizin, uzantilarArr, hedefDizin, format);
+//ProjeKodlariOkuma.ProjeKodlariOkuma.KodlariOLustur(kokDizin, uzantilarArr, hedefDizin, format);
 
-;
+ProjeKodlariOkuma.TaraVeBekle(kokDizin, uzantilarArr);
+
+
+
