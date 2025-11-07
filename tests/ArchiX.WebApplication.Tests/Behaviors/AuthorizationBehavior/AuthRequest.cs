ff681534e6cca2e203459d06ceed00d5ca31c357
Cahit Kalkan <kcahit@gmail.com>
2025-11-08 00:31:40 +0300

    Remove tests/ArchiX.WebApplication.Tests (migrated to ArchiX.Library.Web.Tests)

--- tests/ArchiX.WebApplication.Tests/Behaviors/AuthorizationBehavior/AuthRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿using ArchiX.WebApplication.Abstractions.Authorizations;
-using ArchiX.WebApplication.Abstractions.Interfaces;
-
-namespace ArchiX.WebApplication.Tests.Behaviors.AuthorizationBehavior
-{
-    [Authorize(AuthorizePolicies.AdminOnly)]
-    public sealed record AuthRequest(string Value) : IRequest<string>;
-}
