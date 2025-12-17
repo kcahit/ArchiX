160f2e5bd374b93c56b9cc7df57c8a8f91ac70c6
Cahit Kalkan <kcahit@gmail.com>
2025-12-17 17:23:47 +0300

    17.0600 Dashboard Template WEbHost a geçti. OK

--- src/ArchiX.Library.Web/Templates/Modern/Pages/Login.cshtml.cs
+++ src/ArchiX.Library.Web/Templates/Modern/Pages/Login.cshtml.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+
 using System.ComponentModel.DataAnnotations;
 
 namespace ArchiX.Library.Web.Templates.Modern.Pages
 {
+   
     public class LoginModel : PageModel
     {
         [BindProperty]
@@ -20,16 +22,17 @@ namespace ArchiX.Library.Web.Templates.Modern.Pages
         {
             returnUrl ??= "/Dashboard";
 
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
+            // GEÇÝCÝ OLARAK DOÐRULAMA KONTROLÜ KAPATILDI ÞÝFRE YOK ÞÝMDÝLÝK
+            //if (!ModelState.IsValid)
+            //{
+            //    return Page();
+            //}
 
             // TODO: Authentication logic will be implemented here
             // Temporary: Accept any login for demo purposes
-            
+
             TempData["StatusMessage"] = $"Hoþ geldiniz, {Input.Email}!";
-            
+
             // Redirect to Dashboard (Modern template)
             return Redirect(returnUrl);
         }
