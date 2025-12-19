b8c08eca8215782f5bd1487987389dca343fd873
Cahit Kalkan <kcahit@gmail.com>
2025-12-19 15:37:53 +0300

    #9 Grid Template çalışmasına devam sayfa çalıştı. ama kolon linki çalışmadı

---
+++ src/ArchiX.WebHost/Pages/grid-template-filtreli.cshtml.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ArchiX.WebHost.Pages;
+
+public class GridTemplateFiltreli : PageModel
+{
+#pragma warning disable CA1822 // Mark members as static
+    public void OnGet() { }
+#pragma warning restore CA1822 // Mark members as static
+}
