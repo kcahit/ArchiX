962b324cb0c3a87b2fed3b5e667504f3584c7742
Cahit Kalkan <kcahit@gmail.com>
2025-12-28 22:22:17 +0300

    #15 kombine.cshtml de compenentlerle çalışır hale getirtildi.

--- src/ArchiX.Library.Web/ViewModels/Grid/GridTableViewModel.cs
+++ src/ArchiX.Library.Web/ViewModels/Grid/GridTableViewModel.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-
-namespace ArchiX.Library.Web.ViewModels.Grid;
+﻿namespace ArchiX.Library.Web.ViewModels.Grid;
 
 public class GridTableViewModel
 {
@@ -10,6 +8,7 @@ public class GridTableViewModel
     public string ResetText { get; set; } = "Sıfırla";
     public string AdvancedSearchText { get; set; } = "Gelişmiş Arama";
     public string ExportText { get; set; } = "Aktar";
+    public bool ShowToolbar { get; set; } = true;
     public bool ShowActions { get; set; } = true;
     public IReadOnlyList<GridColumnDefinition> Columns { get; set; } = new List<GridColumnDefinition>();
     public IEnumerable<IDictionary<string, object?>> Rows { get; set; } = new List<IDictionary<string, object?>>();
