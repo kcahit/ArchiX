63c9e81b12c738d35f9cfcf086bf394a07cd85e5
Cahit Kalkan <kcahit@gmail.com>
2025-12-28 21:11:57 +0300

    #15 GridToolbarViewComponent.cs eklendi GRidListe buna göre çalışıyor

---
+++ src/ArchiX.Library.Web/ViewModels/Grid/GridToolbarViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ArchiX.Library.Web.ViewModels.Grid;
+
+public class GridToolbarViewModel
+{
+    public string Id { get; set; } = "gridTable";
+    public int TotalRecords { get; set; } = 0;
+    public string SearchPlaceholder { get; set; } = "Genel arama...";
+    public string ResetText { get; set; } = "Sıfırla";
+    public string AdvancedSearchText { get; set; } = "Gelişmiş Arama";
+    public string ExportText { get; set; } = "Aktar";
+    public bool ShowAdvancedSearch { get; set; } = true;
+    public bool ShowExport { get; set; } = true;
+}
