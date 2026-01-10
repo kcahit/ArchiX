7171f11cd39d5be4e37a6614526e918424329c8d
Cahit Kalkan <kcahit@gmail.com>
2026-01-10 15:11:46 +0300

    #30 warningler düzeldi.

--- src/ArchiX.Library.Web/Pages/Definitions/Application.cshtml.cs
+++ src/ArchiX.Library.Web/Pages/Definitions/Application.cshtml.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System.Diagnostics.CodeAnalysis;
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ArchiX.Library.Web.Pages.Definitions;
 
 public class ApplicationModel : PageModel
 {
+    [SuppressMessage(
+        "Performance",
+        "CA1822:Members that do not access instance data or call instance methods can be marked as static",
+        Justification = "Razor Pages handler methods must be instance methods.")]
     public void OnGet()
     {
     }
